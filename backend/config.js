3ce20c65c41e063319f07a3d4586ac4878e1eae8
lasha-codes <lashagiorgi420@gmail.com>
2024-03-20 22:15:10 -0700

    old i just wanted to commit to repo tho its very very very ez project to do

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555
+
+export const mongoDBUrl =
+  'mongodb+srv://LashaDB:Lasha20091313@cluster0.u0lssov.mongodb.net/?retryWrites=true&w=majority'
---
+++ backend/frontend/src/components/home/BooksCard.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import BooksSingleCard from './BooksSingleCard'
+
+const BooksCard = ({ books }) => {
+  return (
+    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+      {books.map((item) => (
+        <BooksSingleCard key={item._id} book={item} />
+      ))}
+    </div>
+  )
+}
+
+export default BooksCard
+
+// stopLine - 52:33 - Make Book Card a single component
